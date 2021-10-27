23cd7ba5e14c465cafede9eefc2e14908515849f
Ronald Tomaz <ronaldtmprofile@gmail.com>
2021-10-27 16:34:05 -0300

    involved the app in the authentication context and created the navigation routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+
 import { Dashboard } from "./pages/Dashboard";
+import { SignUp } from "./pages/SignUp";
 import { GlobalStyles } from "./styles/Global";
 
-function App() {
+export function App() {
   return (
-    <>
-      <Dashboard />
-      <GlobalStyles />
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <Route path="/" exact component={SignUp} />
+        <Route path="/dashboard" component={Dashboard} />
+        <GlobalStyles />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
-
-export default App;
