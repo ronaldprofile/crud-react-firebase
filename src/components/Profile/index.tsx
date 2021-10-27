7393508d2920a9832f7df1144a06bc715d16b80d
Ronald Tomaz <ronaldtmprofile@gmail.com>
2021-10-27 16:29:28 -0300

    added new component, called Profile

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export function Profile() {
+  return (
+    <Container>
+      <img src="https://github.com/ronaldprofile.png" alt="Ronald Tomaz" />
+    </Container>
+  );
+}
